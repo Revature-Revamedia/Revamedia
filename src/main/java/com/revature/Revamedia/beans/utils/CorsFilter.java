886660911ea1d7b828d5227f7444dbe18075235f
Jarod H <38392785+DuhChosenNoodle@users.noreply.github.com>
2022-06-07 14:17:00 -0700

    now on application v0.3.0
    cors filter allows localhost and aws
    Update pom.xml

--- src/main/java/com/revature/Revamedia/beans/utils/CorsFilter.java
+++ src/main/java/com/revature/Revamedia/beans/utils/CorsFilter.java
@@ -1,6 +1,6 @@
 /**
  * Author(s): @George Henderson
- * Contributor(s):
+ * Contributor(s): @Jarod Heng
  * Purpose: CorsFilter
  */
 package com.revature.Revamedia.beans.utils;
@@ -15,6 +15,9 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
@@ -22,11 +25,18 @@ public class CorsFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-         response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.addHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, HEAD");
-//        response.setHeader("Access-Control-Allow-Origin", "http://localhost:4200"); // Cannot be '*'. If in production we change this to production domain url.
-        response.setHeader("Access-Control-Allow-Origin", "http://220328-revamedia-ui.s3-website-us-east-1.amazonaws.com"); // Cannot be '*'. If in production we change this to production domain url.
-        response.setHeader("Access-Control-Allow-Headers", "Origin, Content-Type, Accept, mode"); // Cannot be '*'. Add additional headers we need here.
-        filterChain.doFilter(request, response);
+        String[] allowDomain = {"http://localhost:4200","http://220328-revamedia-ui.s3-website-us-east-1.amazonaws.com",
+                                "http://revamedia-ui.s3-website-us-west-1.amazonaws.com"};
+        Set<String> allowedOrigins = new HashSet<>(Arrays.asList(allowDomain));
+
+        String originHeader = request.getHeader("Origin");
+
+        if(allowedOrigins.contains(originHeader)) {
+            response.setHeader("Access-Control-Allow-Credentials", "true");
+            response.addHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, HEAD");
+            response.setHeader("Access-Control-Allow-Origin", originHeader); // Cannot be '*'. If in production we change this to production domain url.
+            response.setHeader("Access-Control-Allow-Headers", "Origin, Content-Type, Accept, mode"); // Cannot be '*'. Add additional headers we need here.
+            filterChain.doFilter(request, response);
+        }
     }
 }
