bc1525391e66d170927fb1634e87c9ec38205930
seaboyz <seaboyz@hotmail.com>
2022-06-07 13:25:49 -0500

    Add UserConversationsServiceTest starter

---
+++ src/test/java/com/revature/Revamedia/beans/services/UserConversationsServiceTest.java
@@ -0,0 +1,25 @@
+package com.revature.Revamedia.beans.services;
+
+import org.junit.jupiter.api.Test;
+
+public class UserConversationsServiceTest {
+    @Test
+    void testGetAllConversations() {
+
+    }
+
+    @Test
+    void testGetConversationById() {
+
+    }
+
+    @Test
+    void testSave() {
+
+    }
+
+    @Test
+    void testUpdate() {
+
+    }
+}
