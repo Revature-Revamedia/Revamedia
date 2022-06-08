d0c85d4a486480fe09e22a2a9018546abe930884
GH94GitHub <46788915+GH94GitHub@users.noreply.github.com>
2022-06-08 15:49:23 -0500

    100% Code Coverage for reset.service

--- Revamedia/src/app/Shared/services/reset-service/reset.service.spec.ts
+++ Revamedia/src/app/Shared/services/reset-service/reset.service.spec.ts
@@ -1,16 +1,46 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
+import { NgForm } from '@angular/forms';
+import { of } from 'rxjs';
 
 import { ResetService } from './reset.service';
 
-describe('ResetService', () => {
+fdescribe('ResetService', () => {
   let service: ResetService;
+  let resetUrl: string;
+  let httpClientSpy: {
+    post: jasmine.Spy
+  }
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    httpClientSpy = jasmine.createSpyObj("HttpClient", ['post']);
+
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: HttpClient, useValue: httpClientSpy }
+      ]
+    });
     service = TestBed.inject(ResetService);
+    resetUrl = service.resetUrl;
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('resetPassword should make a post request to proper url', () => {
+    httpClientSpy.post.and.returnValue(of({}));
+    const resetForm = { value: {
+      username: "Johnny44",
+      password: "SecretPassword"
+    }} as NgForm;
+
+    service.resetPassword(resetForm);
+
+    expect(httpClientSpy.post).toHaveBeenCalledWith(
+      resetUrl,
+      { username: "Johnny44", password: "SecretPassword" },
+      { headers: jasmine.any(HttpHeaders), withCredentials: true }
+    )
   });
 });
