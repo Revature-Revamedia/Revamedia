be226b2530bcce26c21add8758df2ce59afbfcce
GH94GitHub <46788915+GH94GitHub@users.noreply.github.com>
2022-06-08 11:44:36 -0500

    100% Code Coverage on animation.service

--- Revamedia/src/app/Shared/services/animation/animation.service.spec.ts
+++ Revamedia/src/app/Shared/services/animation/animation.service.spec.ts
@@ -1,8 +1,9 @@
 import { TestBed } from '@angular/core/testing';
+import gsap from 'gsap';
 
 import { AnimationService } from './animation.service';
 
-describe('AnimationService', () => {
+fdescribe('AnimationService', () => {
   let service: AnimationService;
 
   beforeEach(() => {
@@ -12,5 +13,20 @@ describe('AnimationService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('fadeIn should call gsap.from with appropriate arguments', () => {
+    // Arrange
+    const element = {} as HTMLElement;
+    const duration: number = 1000;
+    const opacity: number = .4;
+    const delay: number = 100;
+    spyOn(gsap, 'from');
+
+    // Act
+    service.fadeIn(element, duration, opacity, delay);
+
+    // Assert
+    expect(gsap.from).toHaveBeenCalled();
   });
 });
