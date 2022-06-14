2092e039c78aa85bbb21b211a8493771f7503798
Brandon Le <61598437+note084@users.noreply.github.com>
2022-06-14 08:13:30 -0700

    bl

--- src/main/java/com/revature/Revamedia/RevamediaApplication.java
+++ src/main/java/com/revature/Revamedia/RevamediaApplication.java
@@ -50,8 +50,8 @@ public class RevamediaApplication {
                 UserGroupsService userGroupsService = context.getBean(UserGroupsService.class);
                 UserConversationsService userConversationsService = context.getBean(UserConversationsService.class);
                 UserMessagesService userMessagesService = context.getBean(UserMessagesService.class);
-//                S3Service S3Service = context.getBean(S3Service.class);
-//                S3Config s3Config = context.getBean(S3Config.class);
+                S3Service S3Service = context.getBean(S3Service.class);
+                S3Config s3Config = context.getBean(S3Config.class);
                 SendEmailService sendEmailService = context.getBean(SendEmailService.class);
                 TwoFactorAuthentication twoFactorAuthentication = context.getBean(TwoFactorAuthentication.class);
 
